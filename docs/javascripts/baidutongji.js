74502040f81a5395ed81cf79d7de39e2fa0cda3f
skyeye <3114284684@qq.com>
2025-08-09 18:13:15 +0800

    小改

--- docs/javascripts/baidutongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?709fff5e06397ccad295c4424900f839";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
